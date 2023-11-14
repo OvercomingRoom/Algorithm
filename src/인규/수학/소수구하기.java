a182c372d76cfa6fc4630b6a7b1da5000b654918
Ingyu Song <ingyu3879@gmail.com>
2023-11-15 00:21:42 +0900

    feat: 소수 구하기

---
+++ src/인규/수학/소수구하기.java
@@ -0,0 +1,31 @@
+package 인규.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 소수구하기 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int M = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(st.nextToken());
+    boolean[] isPrime = new boolean[N + 1];
+    Arrays.fill(isPrime, true);
+    isPrime[1] = false;
+    for (int i = 2; i <= Math.sqrt(N); i++) {
+      for (int j = i * i; j <= N; j += i) {
+        isPrime[j] = false;
+      }
+    }
+
+    for (int i = M; i <= N; i++) {
+      if (isPrime[i]) {
+        System.out.println(i);
+      }
+    }
+  }
+}
