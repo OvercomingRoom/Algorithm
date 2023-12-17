13196554b04549b24092006f1dac2c4ed3908471
Shjung <wjdtngus9@gmail.com>
2023-12-17 19:53:36 +0900

    feat: N과 M (3)

---
+++ src/수현/브루트_포스/N과M_3.java
@@ -0,0 +1,40 @@
+package 수현.브루트_포스;
+
+import java.io.*;
+
+public class N과M_3 {
+
+    static int N, M;
+    static int[] arr;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] split = br.readLine().split(" ");
+
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
+
+        arr = new int[M + 1];
+
+        dfs(0);
+        bw.flush();
+        bw.close();
+    }
+
+    static void dfs(int depth) throws IOException {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+}
