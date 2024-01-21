380d564117f16bb1a21d1bf672da211cc3278727
Shjung <wjdtngus9@gmail.com>
2024-01-21 16:51:29 +0900

    feat : 다음순열

--- src/수현/브루트_포스/순열/다음순열.java
+++ src/수현/브루트_포스/순열/다음순열.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//https://code.plus/course/51
+//https://www.acmicpc.net/problem/10972
 public class 다음순열 {
 
     static int[] numArr, tempArr;
@@ -28,6 +28,8 @@ public class 다음순열 {
             numArr[i] = Integer.parseInt(split[i]);
         }
 
+        tempArr = numArr.clone();;
+
         순열 = 0;
         for (int i = N - 1, digit = 1; i >= 0; i--, digit *= 10) {
             순열 += numArr[i] * digit;
@@ -46,45 +48,56 @@ public class 다음순열 {
 
         findPermutation();
 
-        for (int i = N - 1; i >= 0; i--) {
-            System.out.print(numArr[i] + " ");
+        for (int i = 0; i < N; i++) {
+            System.out.print(tempArr[i] + " ");
         }
 
     }
 
-
     static void findPermutation() {
-
-        for (int i = 순열 + 1; i < 마지막순열; i++) {
-            int temp = i;
-            Arrays.fill(chk, false);
-
-            for (int j = 0; j < N; j++) {
-                tempArr[j] = temp % 10;
-                temp /= 10;
-            }
-
-            boolean findNum = false;
-            int count = 0;
-            for (int j = 0; j < N; j++) {
-                int index = Arrays.binarySearch(numArr, tempArr[j]);
-                if (index >= 0 && !chk[index]) {
-                    chk[index] = true;
-                    findNum = true;
-                    count++;
-                } else if (index < 0 || (index >= 0 && chk[index])) {
-                    findNum = false;
-                    break;
+        int index1, index2, nextNum = 0;
+        for (int i = N - 1; i >= 1; i--) {
+            // 1. 오른쪽 수, 왼쪽 수 비교
+            if (tempArr[i] > tempArr[i - 1]) {
+                // 2. 오른쪽 수가 왼쪽 수 보다 클 때 index1 저장
+                index1 = i - 1;
+                index2 = index1;
+                // 3. tempArr[index1] 보다 큰 수를 다시 오른쪽 부터 찾고, index2 저장
+                nextNum = tempArr[index1];
+                boolean findIndex = false;
+                while (!findIndex) {
+                    nextNum++;
+                    for (int j = index1 + 1; j < N; j++) {
+                        if (tempArr[j] == nextNum) {
+                            index2 = j;
+                            findIndex = true;
+                            // 4. tempArr[index1], tempArr[index2] 서로 swap
+                            swap(index1, index2);
+                            break;
+                        }
+                    }
                 }
-            }
 
-            if (findNum && count == N) {
-                다음순열 = i;
-                numArr = tempArr;
+                // 5. tempArr[index1 + 1] 부터 tempArr[tempArr.length() - 1] 까지 수를 작은 수 부터 swap 시키기.
+                for (int j = index1 + 1; j < N - 1; j++) {
+                    for (int k = j + 1; k < N; k++) {
+                        if (tempArr[j] > tempArr[k]) {
+                            swap(j, k);
+                        }
+                    }
+                }
                 break;
             }
-
         }
+
+
+    }
+
+    static void swap(int index1, int index2) {
+        int temp = tempArr[index1];
+        tempArr[index1] = tempArr[index2];
+        tempArr[index2] = temp;
+
     }
 
 }
