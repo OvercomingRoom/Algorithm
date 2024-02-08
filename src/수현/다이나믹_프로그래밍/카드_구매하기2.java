784f6d5d946acc8450f5f5553d7cb76c9e88d3a0
Shjung <wjdtngus9@gmail.com>
2024-02-08 18:05:19 +0900

    feat : 카드 구매하기2

--- src/수현/다이나믹_프로그래밍/카드_구매하기2.java
+++ src/수현/다이나믹_프로그래밍/카드_구매하기2.java
@@ -21,8 +21,6 @@ public class 카드_구매하기2 {
         for (int i = 2; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j] + cost[j]);
-                // dp[i] 는 카드를 i 개 구매하는데 필요한 최소 비용이 저장
-                // dp[i - j] 는 i - j 개 구매하는데 필요한 최소비용 +cost[j] 를 해서 dp[i] 구매에 필요한 금액을 새롭게 구함.
             }
         }
 
